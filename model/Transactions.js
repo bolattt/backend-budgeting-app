00b9944e4d1fc55826ea3a49b091c7d4945c1dd1
Bo Latt <bolattny@gmail.com>
2023-01-04 20:48:40 -0500

    add seed data

---
+++ model/Transactions.js
@@ -0,0 +1,46 @@
+const transactions = [
+  {
+    name: "income",
+    amount: 2200,
+    date: "April 1",
+    from: "employer",
+    category: "income",
+  },
+  {
+    name: "savings",
+    amount: 500,
+    date: "April 2",
+    from: "bank",
+    category: "saving",
+  },
+  {
+    name: "cat food",
+    amount: 20,
+    date: "April 3",
+    from: "pet store",
+    category: "pets",
+  },
+  {
+    name: "stop and shop",
+    amount: 45,
+    date: "April 4",
+    from: "stop and shop",
+    category: "grocery",
+  },
+  {
+    name: "coffee",
+    amount: 10,
+    date: "April 6",
+    from: "starbucks",
+    category: "food",
+  },
+  {
+    name: "gas",
+    amount: 60,
+    date: "April 12",
+    from: "Exxon mobil",
+    category: "gas",
+  },
+];
+
+module.exports = transactions;
